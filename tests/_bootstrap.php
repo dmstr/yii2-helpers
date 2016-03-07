fd42a8fb8a842124e12f7fea9375498bbee20d61
Tobias Munk <schmunk@usrbin.de>
2016-03-07 09:03:11 +0100

    updated test config

---
+++ tests/_bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+
+require(__DIR__ . '/../../../../vendor/autoload.php');
+require(__DIR__ . '/../../../../src/config/env.php');
+
+if (getenv('YII_ENV') !== 'test') {
+    echo "Error: YII_ENV must be set to 'test'\n";
+    exit;
+}
+
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'test');
+
+defined('YII_TEST_ENTRY_URL') or define('YII_TEST_ENTRY_URL', parse_url(\Codeception\Configuration::config()['config']['test_entry_url'], PHP_URL_PATH));
+defined('YII_TEST_ENTRY_FILE') or define('YII_TEST_ENTRY_FILE', '/app/web/index.php');
+
+require_once(__DIR__ . '/../../../../vendor/yiisoft/yii2/Yii.php');
+
+$_SERVER['SCRIPT_FILENAME'] = YII_TEST_ENTRY_FILE;
+$_SERVER['SCRIPT_NAME'] = YII_TEST_ENTRY_URL;
+$_SERVER['SERVER_NAME'] = parse_url(\Codeception\Configuration::config()['config']['test_entry_url'], PHP_URL_HOST);
+$_SERVER['SERVER_PORT'] =  parse_url(\Codeception\Configuration::config()['config']['test_entry_url'], PHP_URL_PORT) ?: '80';
+
+Yii::setAlias('@tests', dirname(__DIR__));
+
