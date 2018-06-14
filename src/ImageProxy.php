340c5855569763fc7806c8fe7822409ffbce4000
Elias Luhr <e.luhr@herzogkommunikation.de>
2018-06-14 07:33:44 +0200

    fixed image proxy helper

--- src/ImageProxy.php
+++ src/ImageProxy.php
@@ -21,13 +21,13 @@ class ImageProxy
     public static function getFile($imageSource, $preset = null)
     {
         // check if settings components is set
-        if (isset(Yii::$app->settings)) {
+        if (isset(\Yii::$app->settings)) {
             // preset example, when using imageproxy, https://github.com/willnorris/imageproxy#examples
-            return Yii::$app->settings->get('imgBaseUrl', 'app.frontend') .
+            return \Yii::$app->settings->get('imgBaseUrl', 'app.frontend') .
                 $preset .
-                Yii::$app->settings->get('imgHostPrefix', 'app.frontend') .
+                \Yii::$app->settings->get('imgHostPrefix', 'app.frontend') .
                 $imageSource .
-                Yii::$app->settings->get('imgHostSuffix', 'app.frontend');
+                \Yii::$app->settings->get('imgHostSuffix', 'app.frontend');
         }
         return $imageSource;
     }
